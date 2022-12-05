5beb18efca91f0b92449cf513e653ac6858faf86
Kheem Chandra <kheemchandrasingh@gmail.com>
2022-12-05 23:30:31 +0530

    asdfsfd

--- pages/api/content.js
+++ pages/api/content.js
@@ -2,6 +2,9 @@ const fs = require('fs')
 
 export default function handler(req, res) {
     if(req.method === 'GET'){
+        fs.readdir('/tmp', {encoding: 'utf-8'}, (err, files) => {
+            console.log('Files are, ', files)
+        })
         fs.readFile('/tmp/msg.txt', {encoding: 'utf-8'}, (err, data) => {
             if(err){
                 res.status(500).json({err: err.message || 'Something went wrong!'})
