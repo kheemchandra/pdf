07e8e72e052e1722d3c22978dde3db17d910a81a
Kheem Chandra <kheemchandrasingh@gmail.com>
2022-12-05 22:26:02 +0530

    ano

--- core/LicenseKey/LicenseKey.js
+++ core/LicenseKey/LicenseKey.js
@@ -4,7 +4,8 @@
 //---------------------------------------------------------------------------------------
 
 //"Enter your key here. If you don't have it, please go to https://www.pdftron.com/pws/get-key to obtain a demo license or https://www.pdftron.com/form/contact-sales to obtain a production key.
-const LicenseKey = 'demo:ashudhanik00@gmail.com:7a8b85200200000000a1f941a48ba68783e9a31623a5631be4005a7a4c';
+const LicenseKey = 'demo:ashudhanik00@gmail.com:7a458b85200200000000a1f941a48ba68783e9a31623a5631be4005a7a4c';
+// const LicenseKey = 'demo:ashudhanik00@gmail.com:7a8b85200200000000a1f941a48ba68783e9a31623a5631be4005a7a4c';
 if(LicenseKey == 'YOUR_PDFTRON_LICENSE_KEY'){
     throw ('Please enter your license key by replacing \'YOUR_PDFTRON_LICENSE_KEY\' that is assigned to the LicenseKey variable in Samples/LicenseKey/LicenseKey.js. If you do not have a license key, please go to https://www.pdftron.com/pws/get-key to obtain a demo license or https://www.pdftron.com/form/contact-sales to obtain a production key.');
 }
