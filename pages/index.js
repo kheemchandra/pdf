c89f6ac4b2b923eab5845a2cd55ef1f42e06ab6c
Kheem Chandra <kheemchandrasingh@gmail.com>
2022-12-05 22:22:10 +0530

    img

--- pages/index.js
+++ pages/index.js
@@ -1,6 +1,7 @@
 import React, {useState} from 'react'
 import axios from 'axios'
 import Link from 'next/link'
+import Image from 'next/image'
 
 
 export default function HomePage() {
@@ -29,6 +30,7 @@ export default function HomePage() {
   }
 
   return <div>
+    <Image style={{border: '1px solid red', borderRadius: '10px'}} src='/static/dog.png' width={250} height={150}/>
     <form onSubmit={submitHandler}>
       <div>
         <label htmlFor="pdf">Select a pdf</label>
