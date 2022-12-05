fc998ec28101b536c390cffdc4ce849244b99631
Kheem Chandra <kheemchandrasingh@gmail.com>
2022-12-05 23:30:31 +0530

    jkl

--- pages/api/content.js
+++ pages/api/content.js
@@ -2,12 +2,15 @@ const fs = require('fs')
 
 export default function handler(req, res) {
     if(req.method === 'GET'){
+        fs.readdir('/tmp', {encoding: 'utf-8'}, (err, files) => {
+            console.log('Files are, ', files)
+        })
         fs.readFile('/tmp/msg.txt', {encoding: 'utf-8'}, (err, data) => {
             if(err){
                 res.status(500).json({err: err.message || 'Something went wrong!'})
                 throw err 
             }
-            res.status(200).json({message: data})
         })
+        res.status(200).json({message: data})
     }
 }
