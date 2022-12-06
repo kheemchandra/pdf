cbb4af11c7b395f26a9af6643be351d0e895785e
Kheem Chandra <kheemchandrasingh@gmail.com>
2022-12-06 15:01:09 +0530

    asd

--- pages/api/show.js
+++ pages/api/show.js
@@ -12,7 +12,16 @@ export default function handler(req, res) {
         if(err){
             return res.status(500).json({error: err.message || 'Something went wrong'})
         }
-        return res.status(200).json({data: files})
+        files.forEach(f => {
+            if(!f.includes('public'))return;
+            fs.readdir(path.join(process.cwd(), 'public/pdf'), {encoding: 'utf-8'}, (err, f1) =>{
+                if(err){
+                    return res.status(500).json({error: err.message || 'Something went wrong'})
+                }
+
+                return res.status(200).json({data: f1})
+            })
+        })
     })
   
 }
