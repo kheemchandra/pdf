d382b8477ba2138cedc695f48a2ef2de635403cc
Kheem Chandra <kheemchandrasingh@gmail.com>
2022-12-06 12:21:12 +0530

    d

--- pages/api/show.js
+++ pages/api/show.js
@@ -1,48 +1,14 @@
-const fs = require('fs')
-const path = require('path')
+import { readFileSync } from 'fs';
+import path from 'path';
 
 let files = []
-
-function ThroughDirectory(Directory) {
-    fs.readdirSync(Directory).forEach(File => {
-        if(File.includes('node_modules'))return;
-        if(File.startsWith('.'))return;
-        const Absolute = path.join(Directory, File);
-        if (fs.statSync(Absolute).isDirectory()) return ThroughDirectory(Absolute);
-        else return files.push(Absolute);
-    });
-}
+ 
 
 
 
 export default function handler(req, res) {
-    if(req.method === 'GET'){
-        
-        fs.readdir('/', {encoding: 'utf-8'}, (err, files) => {
-            if(err){
-                res.status(500).json({err: 'Something went wrong!'|| err.message})
-            }
-            // files.forEach(f => {
-            //     if(f === 'tmp'){
-            //         res.status(200).json({onm: files})
-            //         fs.readdir(path.join('/', f), {encoding: 'utf-8'}, (err, files1) => {
-            //             if(err){
-            //                 res.status(500).json({err: 'Something went wrong!'|| err.message})
-            //             }
-            //             console.log(files1)
-            //             res.status(200).json({data: files1})
-            //         }
-            //         )
-            //     }
-            // })
-            // console.log('****************')
-            // console.log(path.join('./'))
-            res.status(200).json({data: files})
-
-        })
-
-        // ThroughDirectory('/')
-        // console.log('files are ', files)
-        // res.status(200).json({message: 'Success!', files: files})
-    }
+    const file = path.join(process.cwd(), 'public/static/', 'msg.txt');
+    const data = readFileSync(file, 'utf8');
+  
+    return res.status(200).json({data: data})
 }
