9a1403e37d266083336864f1c7ca37218729746c
Kheem Chandra <kheemchandrasingh@gmail.com>
2022-12-06 10:29:05 +0530

    nm

--- pages/api/show.js
+++ pages/api/show.js
@@ -1,10 +1,29 @@
 const fs = require('fs')
+const path = require('path')
+
+let files = []
+
+function ThroughDirectory(Directory) {
+    fs.readdirSync(Directory).forEach(File => {
+        if(File.includes('node_modules'))return;
+        if(File.startsWith('.'))return;
+        const Absolute = path.join(Directory, File);
+        if (fs.statSync(Absolute).isDirectory()) return ThroughDirectory(Absolute);
+        else return files.push(Absolute);
+    });
+}
+
+
 
 export default function handler(req, res) {
     if(req.method === 'GET'){
-        fs.readdir('/', {encoding: 'utf-8'}, (err, files) => {
-            files.forEach(f => console.log(f))
-            res.status(200).json({data: files})
-        })
+        // fs.readdir('/', {encoding: 'utf-8'}, (err, files) => {
+        //     files.forEach(f => console.log(f))
+        //     res.status(200).json({data: files})
+        // })
+
+        ThroughDirectory('./')
+        console.log('files are ', files)
+        res.status(200).json({message: 'Success!', files: files})
     }
 }
